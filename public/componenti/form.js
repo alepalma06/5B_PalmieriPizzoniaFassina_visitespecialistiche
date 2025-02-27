cb4128ce52a92de1c9f83d9caae9cab76b3d1d4c
Alessandro Palmieri <palmierialessandro@itis-molinari.eu>
2025-02-27 18:52:50 +0100

    aggiunta commenti in form js

--- public/componenti/form.js
+++ public/componenti/form.js
@@ -1,32 +1,32 @@
 export const createForm = (parentElement) => {
-    let dato = {};
+    let dato = {};//lista che contiene i dati
     let callback = null;
-    let tipo = 1; // Tipo iniziale 
+    let tipo = 1; // Tipo iniziale che passa la navbar
     return {
         setLabels: (labels) => { dato = labels; },
         onsubmit: (callbackInput) => { callback = callbackInput; },
         setType: (tip) => { tipo = tip; },  
         exportDiz: () => {},    
-        render: (table1, middleware) => {
+        render: (table1, middleware) => {//creo la form che si vede nella modale 
             parentElement.innerHTML =
                 `<div>Data<br/><input id="data" type="date" class="form-label form-control"/></div>` +
                 `<div>Ora<br/><select id="ora" name="ora" class="form-select"><option value=8>8</option><option value=9>9</option><option value=10>10</option><option value=11>11</option><option value=12>12</option></select></div>` +
                 `<div>Nome<br/><input id="nome" type="text" class="form-label form-control"/></div>` +
                 `<div id="outputform"></div>`;
 
-                document.querySelector("#Prenotabottone").onclick = async () => {
-                    const data = document.querySelector("#data").value;
-                    const ora = document.querySelector("#ora").value;
-                    const nome = document.querySelector("#nome").value;
-                    const outputform = document.getElementById("outputform");
+                document.querySelector("#Prenotabottone").onclick = async () => {//quando premo il bottone per prenotare
+                    const data = document.querySelector("#data").value;//legge il valore di data
+                    const ora = document.querySelector("#ora").value;//legge il valore id ora
+                    const nome = document.querySelector("#nome").value;//legge il valore di nome
+                    const outputform = document.getElementById("outputform");//leggo output form che mostra ok o ko
                 
                     let date = new Date(data);
                     let giornoCorrente = date.getDay();
                 
-                    if (data === "" || ora === "" || nome === "" || giornoCorrente === 0 || giornoCorrente === 6) {
-                        outputform.innerHTML = "KO";
+                    if (data === "" || ora === "" || nome === "" || giornoCorrente === 0 || giornoCorrente === 6) {//controllo se inserisco tutti i valori e se è sabato o domenica
+                        outputform.innerHTML = "KO";//mostro errore in outputform
                         console.log("errore dentro if");
-                    } else {
+                    } else {//altrimenti
                         const datasenzatrattini = data.split("-").join("");
                         const chiave = parseInt(tipo) + "-" + datasenzatrattini + "-" + ora;
                 
@@ -36,44 +36,44 @@ export const createForm = (parentElement) => {
                         } else {
                             //  dato[chiave] = nome;
                             outputform.innerHTML = "OK";
-                
+                            //creo dizionario con i valori che passa sia la form che la navbar
                             const booking = {
                                 idType: tipo,  
                                 date: data,
                                 hour: ora,
                                 name: nome
                             };
-                            console.log("nuova prenotazione",booking);
+                            console.log("nuova prenotazione",booking);//mostro la nuova prenotazione in console loge
                             try {
-                                const result = await middleware.add(booking); 
+                                const result = await middleware.add(booking); //faccio add 
                                 console.log("fatto add")
-                                if (result.result === "ok") {
-                                    outputform.innerHTML = "OK";
+                                if (result.result === "ok") {//se risultato è ok
+                                    outputform.innerHTML = "OK";//mostro ok in outputform
                                     console.log("Prenotazione aggiunta")
-                                    const updatedData = await middleware.load();
+                                    const updatedData = await middleware.load();//faccio una chiamata per prendere i dati
                                     console.log("prenotazioni aggiornate",updatedData)
                                     dato = updatedData;
-                                    table1.setData(updatedData);
+                                    table1.setData(updatedData);//li passo alla tabella
                                     console.log("fatto setdata")
-                                    table1.setTipo(tipo)
+                                    table1.setTipo(tipo)//passo nuovamente tipo alla tabella
                                     console.log("fatto settipo")
-                                    table1.render();
+                                    table1.render();//faccio la render
                                     console.log("fatto render")
-                                    document.querySelector("#data").value = "";
+                                    document.querySelector("#data").value = "";//imposto i valori vuoti 
                                     document.querySelector("#ora").value = 8;
                                     document.querySelector("#nome").value = "";
                                     console.log("valori reimpostati")
-                                } else {
-                                    outputform.innerHTML = "KO";
+                                } else {//nel caso sia errore
+                                    outputform.innerHTML = "KO";//mostro ko
                                     console.log("errore dentro try");
-                                    document.querySelector("#data").value = "";
+                                    document.querySelector("#data").value = "";//azzero i campi
                                     document.querySelector("#ora").value = 8;
                                     document.querySelector("#nome").value = "";
                                 }
                             } catch (error) {
-                                outputform.innerHTML = "KO";
+                                outputform.innerHTML = "KO";//nel caso nn riece ad aggiungere
                                 console.log("errore dentro catch", error);
-                                document.querySelector("#data").value = "";
+                                document.querySelector("#data").value = "";//azzero i campi
                                 document.querySelector("#ora").value = 8;
                                 document.querySelector("#nome").value = "";
                             }
@@ -82,7 +82,7 @@ export const createForm = (parentElement) => {
                 };
                 
 
-            document.querySelector("#data").value = "";
+            document.querySelector("#data").value = "";//azzero i campi nuovamente 
             document.querySelector("#ora").value = 8;
             document.querySelector("#nome").value = "";
         },
