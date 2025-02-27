576c4db2edbce19570f0f30de6169cbe0bfd645e
Alessandro Palmieri <palmierialessandro@itis-molinari.eu>
2025-02-27 08:36:25 +0100

    modifica server

--- server.js
+++ server.js
@@ -1,80 +1,35 @@
-const createMiddleware = () => {
-    return {
-        load: async () => {
-            const response = await fetch("/booking");
-            return await response.json();
-        },
-        delete: async (id) => {
-            const response = await fetch("/delete/" + id, {
-                method: 'DELETE',
-            });
-            return await response.json();
-        },
-        add: async (booking) => {
-            const response = await fetch("/insert", {
-                method: 'POST',
-                headers: {
-                    "content-type": "application/json"
-                },
-                body: JSON.stringify({ booking: booking })
-            });
-            return await response.json();
-        }
-    };
-  };
-  
-  export default createMiddleware;
-  
-import { tableComponent } from './componenti/tabella.js';
-import { NavBarComponent } from './componenti/navbar.js';
-import { createForm } from './componenti/form.js';
+const express = require("express");
+const http = require('http');
+const path = require('path');
+const app = express();
+const database = require("./database");
+database.createTable();
 
-const tabella = document.getElementById("tabella");
-const precendente = document.getElementById("precedente");
-const successiva = document.getElementById("successiva");
-let starDay = 0;
-const navbar = document.getElementById("navbar");
-const formElement = document.getElementById("form");
-
-const middleware = createMiddleware();
-const navBarComp = NavBarComponent();
-const form = createForm(formElement);
-const table1 = tableComponent();
-
-fetch("/types")
-  .then(response => response.json())
-  .then(types => {
-    navBarComp.setParentElement(navbar);
-    navBarComp.render(form, table1, types);
-
-    // Carica tutte le prenotazioni
-    return middleware.load();
-  })
-  .then(data => {
-    console.log("Dati caricati:", data);
-    // Gestisci i dati delle prenotazioni mantenendo idType
-    form.setLabels(data);
-    console.log("Dati form:", data[0]);
-    table1.setData(data);  // Passa tutte le prenotazioni al componente tabella
-    table1.setParentElement(tabella);
-    table1.render();
-
-    // Gestisci i cambiamenti tra settimane
-    precendente.onclick = () => {
-      starDay -= 7;
-      table1.start(starDay);
-      table1.render();
-    };
-
-    successiva.onclick = () => {
-      starDay += 7;
-      table1.start(starDay);
-      table1.render();
-    };
-
-    // Rende il form per aggiungere prenotazioni
-    form.render(table1, middleware);
-  })
-  .catch(error => console.error("Errore nel caricamento dei dati:", error));
-
-
+app.use(express.json()); 
+app.use("/", express.static(path.join(__dirname, "public")));
+app.post("/insert", async (req, res) => {
+  const booking = req.body.booking;
+  try {
+    await database.insert(booking);
+    res.json({result: "ok"});
+  } catch (e) {
+    res.status(500).json({result: "ko"});
+  }
+})
+app.get('/booking', async (req, res) => {
+    const list = await database.select();
+    res.json(list);
+});
+app.get('/types', async (req, res) => {
+    const types = await database.getTypes();
+    res.json(types);
+});
+app.delete('/delete/:id', async (req, res) => {
+  await database.delete(req.params.id);
+  res.json({result: "ok"});
+})
+const server = http.createServer(app);
+const port = 5600;
+server.listen(port, () => {
+  console.log("- server running on port: " + port);
+});
